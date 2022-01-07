6f3dcb46877942c1d98dc7eb9c1efa4d48c7185b
DrunkBatya <drunkbatya.js@gmail.com>
2022-01-08 00:16:25 +0300

    some fixes e.g. db_getchar now returns -1 on EOF

--- c/count1/count1.c
+++ c/count1/count1.c
@@ -6,33 +6,52 @@
 /*   By: drunkbatya <drunkbatya.js@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 13:20:40 by drunkbaty         #+#    #+#             */
-/*   Updated: 2022/01/06 14:20:17 by drunkbaty        ###   ########.fr       */
+/*   Updated: 2022/01/08 00:10:31 by drunkbaty        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <inttypes.h>
+#include "../putstr/db_putstr.h"
+#include "../putnbr/db_putnbr.h"
+#include "../getchar/db_getchar.h"
+
+void	print_stat(int sym, int nl, int space, int tab)
+{
+	db_putstr("Total:\n\tSymbols: ");
+	db_putnbr(sym);
+	db_putstr("\n\tSpaces: ");
+	db_putnbr(space);
+	db_putstr("\n\tTabs: ");
+	db_putnbr(tab);
+	db_putstr("\n\tLines: ");
+	db_putnbr(nl);
+	db_putstr("\nEND\n");
+}
 
 int	main(void)
 {
-	uint32_t	nl;
-	uint32_t	space;
-	uint32_t	tab;
-	uint32_t	sym;
-	char		c;
+	int		nl;
+	int		space;
+	int		tab;
+	int		sym;
+	char	ch;
 
 	nl = 0;
 	space = 0;
 	tab = 0;
-	for (sym = 0; (c = getchar()) != EOF; sym++)
+	sym = -1;
+	while (ch != EOF)
 	{
-		if (c == '\n')
+		ch = db_getchar();
+		if (ch == '\n')
 			nl++;
-		if (c == ' ')
+		else if (ch == ' ')
 			space++;
-		if (c == '\t')
+		else if (ch == '\t')
 			tab++;
+		sym++;
 	}
-	printf("Total:\n\tSymbols: %d\n\tSpaces: %d\n\tTabs: %d\n\tLines: %d\nEND\n", sym, space, tab, nl);
+	print_stat(sym, nl, space, tab);
 	return (0);
 }
--- c/test/main.c
+++ c/test/main.c
@@ -1,24 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   db_getchar.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: drunkbatya <drunkbatya.js@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/06 13:25:17 by drunkbaty         #+#    #+#             */
-/*   Updated: 2022/01/07 23:56:33 by drunkbaty        ###   ########.fr       */
+/*   Created: 2022/01/07 17:09:28 by drunkbaty         #+#    #+#             */
+/*   Updated: 2022/01/08 00:14:47 by drunkbaty        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
+#include "../getchar/db_getchar.h"
 
-char	db_getchar(void)
+int	main(void)
 {
-	int		size;
+	int		i;
 	char	ch;
 
-	size = read(0, &ch, 1);
-	if (size <= 0)
-		ch = -1;
-	return (ch);
+	i = 0;
+	while (i++ < 5)
+	{
+		ch = db_getchar();
+		printf("%d\n", ch);
+	}
+	return (0);
 }
